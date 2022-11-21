3438f9d901b33d9110afc7a9d8989e997590474c
Sourabh Gurwani <sonusourabh2411@gmail.com>
2022-11-21 19:14:40 +0530

    pb

---
+++ lists.method.py
@@ -0,0 +1,20 @@
+l1=[1,8,7,2,21,20]
+
+l1_sorted=l1.sort()  #l1.sort() = arranging the list values in increasing order
+print(l1) 
+
+l1_reverse=l1.reverse() #l1.reverse() = decreasing value of list
+print(l1)
+
+l1.append(24) #adds 24 at the end
+print(l1)
+
+l1.insert(2,240) #insert 240 at index 2
+print(l1)
+
+l1.pop(3) #will delete the value at index 3
+print(l1)
+
+
+l1.remove(20) #remove 20 from list
+print(l1)
